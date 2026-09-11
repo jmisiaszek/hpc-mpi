0470cd7d9a62ff19d1bac6a619f3bbf74803c5f0
Jakub Misiaszek <jmisiaszek3@gmail.com>
2026-09-12 01:33:09 +0200

    fix

--- init2.sh
+++ init2.sh
@@ -3,9 +3,9 @@ mkdir -p logs_1node logs_2node
 for P in 16 32 48; do
     sbatch --ntasks=$P --nodes=1 \
            --output=logs_1node/place_P${P}_j%j.out \
-           jobs/run_offset_sweep.sh
+           jobs/run_offset.sh
 
     sbatch --ntasks=$P --nodes=2 --ntasks-per-node=$((P/2)) \
            --output=logs_2node/place_P${P}_j%j.out \
-           jobs/run_offset_sweep.sh
+           jobs/run_offset.sh
 done
